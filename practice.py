378cdc1f9500a9db1aa3c597c6d207096e723047
sukhveeratwal24 <sukhveer_atwal@live.co.uk>
2021-03-25 12:06:54 +0000

    commit on main

---
+++ practice.py
@@ -0,0 +1,115 @@
+#number1 = 22
+#number2 = 33
+
+#total = number1 + number2
+
+#print(total)
+
+# input function- add a question and what they come back (return) with is a string
+# x = int(input("Please enter a number "))
+# y = int(input("Please enter a number "))
+# only if int is used will there be a return
+# print(x+y)
+
+# z = input ("What is your name ")
+# print("Hi " + z)
+
+# Floats are decimal points 
+# Booleans are true or false
+
+# x = input("Add two numbers ")
+# y = input("add another number ")
+# total = int(x) + int(y)
+# print(total)
+
+## / Divide with remainder and // whole number
+## print(7//5, "r", 7%5)
+
+#x = int(input("Please enter the first number please "))
+#y = int(input("Please enter the second number please "))
+#z = int(input("Please enter the third number please "))
+#total = (x+y+z)
+#print(total)
+#print("total " + str(total/3))
+
+# Loops
+
+#print (range (10)) # this is a range from 0-9
+
+#for counter in [1,2,3,4]: # in square brackets is a list
+    #print(counter)
+    #print(counter+4)
+
+#print("end")
+
+#loop
+#text = input("Share a message ")
+#number = int(input("How many times would you like to repeat this? "))
+#for i in range (number):
+    #print(text)
+
+#text = int(input("which times table would you like to see?"))
+#for i in range (1,13):
+    #print(i*text)
+    #print(str(i) + "x" + "=" + str(i*text))
+
+# Selection- assigntment operators
+#x = 23
+#x += 10
+#print(x)
+
+#y = 23
+#y -= 10
+#print(y)
+
+#z = 23
+#z %= 10
+#print(z)
+
+#realtional make sure you put query in brackets- to see 
+# if some thing is true false you can use and not or
+#x = 5
+#print(not (x<1))
+
+#x = 5
+#if x>4:
+    #print("yes")
+
+#print("end")
+
+# conditions 
+#number1 = int(input("select one number "))
+#number2 = int(input("select a second number "))
+#if number1<number2:
+    #print("number 1 is smaller than the second number")
+#elif number1==number2: #need to define the condition as elif not else and can only use else once at end
+    #print("they are the same")
+#else:
+    #print("number 1 is bigger than the second number")
+#print("end")
+
+#year = int(input("what year were you born in?"))
+#x = year%4
+
+#if x==0:
+    #print("yes")
+#else:
+    #print("no")
+
+##nested statements
+#x = input("are you a pensioner, teenager or adult? ")
+#if x == "pensioner" or x == "teenager": 
+    #print("entry is free")
+#else:
+    #y = input("do you have a swimcard? ")
+    #if y == "yes":
+        #print("you get 50% off")
+    #else:
+        #print("pay £5")
+
+#count = 1
+
+#while count<10:
+    #print(count)
+    #count+=1
+
